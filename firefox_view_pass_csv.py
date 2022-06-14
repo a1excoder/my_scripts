b8bd7ebddd930336c44f9ead94e3b9dd9437973b
Aleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2022-06-14 15:35:42 +0300

    Add files via upload

---
+++ firefox_view_pass_csv.py
@@ -0,0 +1,20 @@
+import csv, sys
+
+if len(sys.argv) < 2:
+	print("Enter csv file name after .py")
+	sys.exit(1)
+
+fname = sys.argv[1]
+
+try:
+	csv_file = open(fname, 'r')
+except FileNotFoundError:
+	print(f"file '{fname}' not found")
+	sys.exit(2)
+
+csv_file_reader = csv.DictReader(csv_file)
+
+for row in csv_file_reader:
+	print(f"{row['url']} | {row['username']} | {row['password']}")
+
+csv_file.close()
